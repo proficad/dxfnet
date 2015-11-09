42fce1dd9ec304b9527109edfb3818411178b920
vjedlicka <proficad@gmail.com>
2015-11-09 12:47:19 +0100

    added tester project

---
+++ Tester/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Loader;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int li_i = 0; li_i < 50; ++li_i)
+            {
+                Console.WriteLine("{0} -> {1}", li_i, Exporter.Calculate_Line_Thickness_Ellipse(li_i));
+            }
+            
+
+
+
+
+
+
+        }
+    }
+}
