7102121edb68adcf96b789726d7544762404b702
Václav Jedlička <proficad@gmail.com>
2016-06-08 13:59:03 +0200

    renamed DxfNet.dll to Core.dll

--- DxfNet/Properties/AssemblyInfo.cs
+++ DxfNet/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DxfNet")]
+[assembly: AssemblyTitle("Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ProfiCAD")]
-[assembly: AssemblyProduct("DxfNet")]
-[assembly: AssemblyCopyright("ProfiCAD ©  2011")]
+[assembly: AssemblyProduct("Core")]
+[assembly: AssemblyCopyright("ProfiCAD ©  2016")]
 [assembly: AssemblyTrademark("ProfiCAD")]
 [assembly: AssemblyCulture("")]
 
--- Loader/Properties/AssemblyInfo.cs
+++ Loader/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ProfiCAD")]
 [assembly: AssemblyProduct("Loader")]
-[assembly: AssemblyCopyright("ProfiCAD ©  2012")]
+[assembly: AssemblyCopyright("ProfiCAD ©  2016")]
 [assembly: AssemblyTrademark("ProfiCAD")]
 [assembly: AssemblyCulture("")]
 
