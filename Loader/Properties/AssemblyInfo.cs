e283fde9b0b51449c566489ccebd758cc8586364
vjedlicka <proficad@gmail.com>
2019-12-28 13:10:31 +0100

    upd ver

--- Loader/Properties/AssemblyInfo.cs
+++ Loader/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ProfiCAD")]
 [assembly: AssemblyProduct("Loader")]
-[assembly: AssemblyCopyright("ProfiCAD ©  2016")]
+[assembly: AssemblyCopyright("ProfiCAD ©  2021")]
 [assembly: AssemblyTrademark("ProfiCAD")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.10.0.1")]
-[assembly: AssemblyFileVersion("3.10.0.1")]
+[assembly: AssemblyVersion("3.10.2.1")]
+[assembly: AssemblyFileVersion("3.10.2.1")]
