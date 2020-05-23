1f87a8f35fdef14d1e6904502c39adb4a03780f1
vjedlicka <proficad@gmail.com>
2020-05-23 11:15:14 +0200

    added QCircle

---
+++ Core/QCircle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+
+namespace DxfNet
+{
+    class QCircle
+    {
+        Point m_center;
+        Point m_tangent;
+
+        public EFont m_efont;
+
+        int m_turns;
+    }
+}
