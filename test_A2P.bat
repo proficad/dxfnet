5eb57fdc0f97d2c802d2b059a5a8d33f7406e464
vjedlicka <proficad@gmail.com>
2021-09-20 19:32:00 +0200

    added testers

---
+++ test_A2P.bat
@@ -0,0 +1,16 @@
+echo off
+
+set outpath = %~dp0A2P\out\in.ppd
+
+
+rem if exist outpath del outpath
+
+A2P "%~dp0A2P\in\in.dxf" "%~dp0A2P\out" ppd yes 20 "c:\temp\nologA2P.txt"
+
+if exist %~dp0A2P\out\in.ppd (
+	echo A2P passed
+) else (
+	echo A2P failed !!!
+)
+
+
